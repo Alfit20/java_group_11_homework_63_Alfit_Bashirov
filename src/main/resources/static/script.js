ede70a985f1c156c3bd5d3f1718b6a3c8de4eb25
BashirovAlfit <alfit.bashirov@gmail.com>
2022-04-28 19:43:28 +0600

    Added authorization with microgram

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -40,6 +40,66 @@ async function createUser(userFormData) {
     console.log(responseData);
 }
 
+// Авторизация по микрограмму запустите программу
+const authUrl = "/user/login"
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', onLoginHandler);
+
+function onLoginHandler(e) {
+    e.preventDefault();
+    let form = e.target;
+    let userFormData = new FormData(form);
+    let user = {
+        username: userFormData.get("email"),
+        password: userFormData.get("password")
+    }
+    localStorage.setItem('user', JSON.stringify(user));
+    let auth = authentication()
+}
+
+
+let getAuth = function () {
+    let userFromStorage = localStorage.getItem("user")
+    let userJson = JSON.parse(userFromStorage)
+    return userJson
+}
+
+async function authentication() {
+    let userAuth = getAuth()
+    // console.log(userAuth.username + ":" + userAuth.password)
+    let settings = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Basic " + btoa(userAuth.username + ":" + userAuth.password),
+            // "Access-Control-Allow-Origin": "*",
+            // "Access-Control-Allow-Methods": "*",
+            // "Access-Control-Allow-Headers": "*"
+
+        },
+        mode: "cors"
+    }
+    try {
+        let searchedUser = await fetch(baseUrl + authUrl, settings).then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            hideSplashScreen()
+            let email = document.createElement("span")
+            email.id = "name"
+            email.setAttribute("style", "color: #fff; text-align: right;")
+            let button = document.getElementById("closeButton")
+            button.before(email)
+            email.append(userAuth.username)
+            return response.text();
+        })
+        console.log(searchedUser)
+    } catch (error) {
+        alert("Неверный логин или пароль")
+    }
+
+}
+
 
 
 
